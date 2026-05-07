faf1ea71f690524c48b67858e739d81ebd2e55ad
hafsa khan waseem <hafsakhanwaseem007@gmail.com>
2026-05-07 21:14:25 +0500

    add index and css and assets

---
+++ assets/js/first.js
@@ -0,0 +1,34 @@
+// age="89";
+// console.log(age);
+// fullname="hafsa";
+// console.log(fullname);
+// price=67;
+// console.log(price);
+// x=null;
+// console.log("null");
+// y= undefined;
+// console.log(y);
+
+// let name="hafsa";
+// alert("hrehe");
+
+
+
+// let name="hafsa";
+// alert("hafsa");
+// let age="20";
+// console.log(age);
+
+// let age="20";
+// let city="karachi";
+// console.log(age);
+// console.log(city);
+// age="hehehe";
+// console.log(age);
+// price=56;
+// console.log(price);
+// name="hafsahafsa";
+// console.log(name);
+
+// alert("i love java-script")
+// document.getElementById("demo").innerHTML="welcome in js";
